8f887119a3fb3bba1e3b03044c67441d395962c6
VUYOLWETHU <VUYOLWETHU@Vuolwethu>
2021-04-01 16:18:15 -0700

    added comments on my branch added a subtracting sum

--- src/test/java/za/ac/cput/adp2assignment1/CalculatorTest.java
+++ src/test/java/za/ac/cput/adp2assignment1/CalculatorTest.java
@@ -44,7 +44,7 @@ public class CalculatorTest {
     @Test
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
-       // fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
     }
     @Test
     public void testAddition(){
